4c570cceeb4b2fa62c782e1563e93ea087e49d52
spflueger <spfluege@gmail.com>
2020-05-28 12:48:46 +0200

    ci: add pypi publish workflow on github actions (#2)

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+def long_description():
+    """Parse long description from readme."""
+    with open("README.md", "r") as readme_file:
+        return readme_file.read()
+
+
+setuptools.setup(
+    name="amplitf",
+    version="0.0-alpha1",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ComPWA/AmpliTF",
+    packages=setuptools.find_packages(),
+    license="GPLv3 or later",
+    python_requires=">=3.6",
+    install_requires=[
+        "iminuit",
+        "numpy",
+        "sympy",
+        "tensorflow>=2.0"
+    ],
+    package_data={},
+    include_package_data=True,
+)

f4a49e50e7aac8c754f66bb09ae663538bce3c99
Remco de Boer <redeboer@gmx.com>
2021-09-06 15:33:44 +0200

    build: build with setuptools_scm + setup.cfg

--- setup.py
+++ setup.py
@@ -1,32 +1,6 @@
-"""
-A setuptools based setup module.
+from setuptools import setup
 
-See:
-https://packaging.python.org/guides/distributing-packages-using-setuptools/
-"""
-
-import setuptools
-
-
-def long_description():
-    """Parse long description from readme."""
-    with open("README.md", "r") as readme_file:
-        return readme_file.read()
-
-
-setuptools.setup(
-    name="amplitf",
-    author="Anton Poluektov",
-    version='0.0a0',
-    long_description=long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/apoluekt/AmpliTF",
-    packages=setuptools.find_packages(),
-    python_requires='>=3.6',
-    install_requires=[
-        'iminuit',
-        'numpy',
-        'sympy',
-        'tensorflow>=2.0',
-    ],
+setup(
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
 )
